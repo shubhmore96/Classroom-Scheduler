6e7c6909b6e7b829eeccf1de93736f8aaa95f3f7
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2022-09-16 22:25:03 +0530

    Calender added

--- Code/FrontEnd/classroom-scheduler/src/Routes/App2.jsx
+++ Code/FrontEnd/classroom-scheduler/src/Routes/App2.jsx
@@ -8,33 +8,34 @@ import AdminSignUp from "../Main/SignUp/AdminSignUp";
 import ForgotPassword from "../Main/Login/ForgotPassword";
 // import { Provider } from "react-redux";
 // import store from './Store/reducer';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AdminDashboard from "../Dashboard/AdminDashboard/AdminDashboard";
 import Login from "../Main/Login/Login";
+import Calender from "../Dashboard/Calender/Calender";
 
-
-const App2=()=>{
-    return(
-        <>
-    <BrowserRouter>
-    <ToastContainer position="top-center" />
-    {/* <Provider store={store}> */}
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route index element={<App />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/AdminLogin" element={<AdminLogin />} />
-        <Route path="/TeacherLogin" element={<TeacherLogin />} />
-        <Route path="/StudentLogin" element={<StudentLogin />} />
-        <Route path="/AdminSignUp" element={<AdminSignUp />} />
-        <Route path="/ForgotPassword" element={<ForgotPassword />} />
-        <Route path="/AdminDashboard" element={<AdminDashboard />} />
-      </Routes>
-      {/* </Provider> */}
-    </BrowserRouter>
-        </>
-    )
-}
+const App2 = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <ToastContainer position="top-center" />
+        {/* <Provider store={store}> */}
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route index element={<App />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/AdminLogin" element={<AdminLogin />} />
+          <Route path="/TeacherLogin" element={<TeacherLogin />} />
+          <Route path="/StudentLogin" element={<StudentLogin />} />
+          <Route path="/AdminSignUp" element={<AdminSignUp />} />
+          <Route path="/ForgotPassword" element={<ForgotPassword />} />
+          <Route path="/AdminDashboard" element={<AdminDashboard />} />
+          <Route path="/Calender" element={<Calender />} />
+        </Routes>
+        {/* </Provider> */}
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App2;
